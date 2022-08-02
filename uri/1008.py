634cffd37a13fe9ea135895a6c0fd377fa06ad08
Rahidul Islam <rahiseli@outlook.com>
2022-08-02 09:36:39 +0600

    1008 & 1009 are accepted

--- uri/1008.py
+++ uri/1008.py
@@ -3,5 +3,5 @@ number = int(input())
 amount = int(input())
 hours = float(input())
 
-print(f"Number = {number}")
-print(f"Amount = U${(amount * hours):.2f}")
+print(f"NUMBER = {number}")
+print(f"SALARY = U$ {(amount * hours):.2f}")
--- uri/1009.py
+++ uri/1009.py
@@ -3,4 +3,4 @@ employee_name = input()
 salary = float(input())
 bonus = (float(input()) * 15) / 100
 total = salary + bonus
-print(f"{employee_name}'s TOTAL = R${total:.2f}")
+print(f"TOTAL = R$ {total:.2f}")
