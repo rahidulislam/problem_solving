d8c09a3b6c4bcfbdc9450bb85f1c0b7172b28370
Rahidul Islam <rahiseli@outlook.com>
2022-08-22 11:24:19 +0600

    1038, 1040 are accepted

---
+++ uri/1038.py
@@ -0,0 +1,15 @@
+"""
+Snack
+Using the following table, write a program that reads a code and the amount of an item. After, print the value to
+pay. This is a very simple program with the only intention of practice of selection commands.
+
+Input The input file contains two integer numbers X and Y. X is the product code and Y is the quantity of this item
+according to the above table.
+
+Output
+The output must be a message "Total: R$ " followed by the total value to be paid, with 2 digits after the decimal point.
+"""
+x, y = list(map(int, input().split()))
+price = [0, 4.00, 4.50, 5.00, 2.00, 1.50]
+pay = price[x] * y
+print(f"Total: R$ {pay:.2f}")
