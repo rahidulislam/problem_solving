4fc4f207078022215b42dfc7033b348c907f22ea
Rahidul Islam <rahiseli@outlook.com>
2022-11-01 09:44:44 +0600

    1048 submit issue is fixed

--- uri/1048.py
+++ uri/1048.py
@@ -3,7 +3,7 @@
 salary = float(input())
 if salary <= 400.00:
     percentage = 15
-elif 400.01 <= salary <= 800.00 :
+elif 400.01 <= salary <= 800.00:
     percentage = 12
 
 elif 800.01 <= salary <= 1200.00:
@@ -12,9 +12,9 @@ elif 1200.01 <= salary <= 2000.00:
     percentage = 7
 else:
     percentage = 4
-earning = (salary*percentage)/100
+earning = (salary * percentage) / 100
 new_salary = salary + earning
 
-print(f''' Novo salario: {new_salary:.2f}
+print(f'''Novo salario: {new_salary:.2f}
 Reajuste ganho: {earning:.2f}
-Em percentual: {percentage:.2f} %''')
+Em percentual: {percentage} %''')
--- uri/test.py
+++ uri/test.py
@@ -1,4 +1,9 @@
-email = input().split('@')
-username = email[0]
-print(email)
-print(username)
+# email = input().split('@')
+# username = email[0]
+# print(email)
+# print(username)
+fruit = {"apple": 10, "orange": 20, "banana": 30, "mango": 50}
+print("Fruit: ", fruit)
+pop_fruit = fruit.pop("mango")
+print("Pop fruit", pop_fruit)
+print("Fruit: ", fruit)
